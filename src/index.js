5a9b3ca3b94035e7eabcdfcfb46a51ccdadc25e0
Gaspard Bariller <gaspard.bariller@tutanota.com>
2019-01-30 17:02:14 +0100

    End of the day commit

--- src/index.js
+++ src/index.js
@@ -31,15 +31,21 @@
     /**
      * Create a new HTMLElement and append it in the given parent.
      * 
-     * @description Creates a HTML tag using the first parameter. If the second parameter is set, append text inside this tag. If the third parameter is set, append the created tag inside the parent.
+     * @description Creates a HTML tag using the first parameter. If the second
+     * parameter is set, append text inside this tag. If the third parameter is
+     * set, append the created tag inside the parent.
      * @example insertElement("a", "Click me!", ".a-container");
      * @param {string} tag The HTML tag to create.
-     * @param {string} text The text that will be displayed inside the tag. E.g. <a>Text</a>.
-     * @param {string} parent The parent of the created tag. The tag will be inserted into this parent.
+     * @param {string} text The text that will be displayed inside the tag.
+     * E.g. <a>Text</a>.
+     * @param {string} parent The parent of the created tag. The tag will be
+     * inserted into this parent.
+     * @returns {HTMLElement} The created HTMLElement.
      */
     function insertElement(tag, text, parent) {
         if (!tag || "string" !== typeof tag) {
-            throw new Error("insertElement requires a tag\n" + tag + " is not allowed as a tag parameter");
+            throw new Error("insertElement requires a tag\n" + tag
+                + " is not allowed as a tag parameter");
         }
         tag = document.createElement(tag);
         if ("undefined" !== typeof text) {
@@ -68,10 +74,12 @@
             menuItem.setAttribute("class", "nav-item");
             menuItem.appendChild(menuLink);
         }
+        return true;
     }
 
     /**
-     * Display the upload button and the title and add a onclick event to the button.
+     * Display the upload button and the title and add a onclick event to the
+     * button.
      *
      * @description //TODO
      * @example //TODO
@@ -79,13 +87,12 @@
      * @param {number} max The maximum number of images accepted in the gallery.
      */
     function displayUpload(images, max) {
-        var uploadContainer = document.querySelector(".uploadContainer");
-        var uploadButton = insertElement("a", "", ".uploadContainer");
+        var uploadContainer = document.querySelector(".js-upload-container");
+        var uploadButton = insertElement("a", "", ".js-upload-container");
         var uploadButtonText = document.createTextNode("upload");
         var form = insertElement("form");
         var inputField = insertElement("input");
         var submitField = insertElement("input");
-
         uploadButton.role = "button";
         uploadButton.className = "btn--success";
         form.action = "";
@@ -94,61 +101,91 @@
         inputField.required = "required";
         inputField.name = "url";
         // TODO : Remove temp value
-        inputField.value = "http://www.ecologytheme.com/wp-content/uploads/2016/06/Top-10-Image-Placeholder-Services-1.jpg";
+        inputField.value = "http://2.bp.blogspot.com/-OML9r4YJQ-s/ULPTtNzja6I/AAAAAAAAB60/5BJwwClFfEU/s1600/bleh.jpg";
         submitField.type = "submit";
         submitField.value = "envoyer";
-        submitField.onclick = function () {
-            sendUrl(form);
-            return false;
-        };
-
         if (images.length > max) {
             uploadButton.className = "btn--warning";
             uploadButtonText = document.createTextNode("supprimer une image");
         }
-
         uploadButton.appendChild(uploadButtonText);
         form.appendChild(inputField);
         form.appendChild(submitField);
         uploadContainer.appendChild(form);
-
-        sendFile(uploadButton);
+        onFileChange(uploadButton);
+        onFormSubmit(form);
     }
 
     /**
-     * Sends the file to the web service.
+     * On file change, send the file to the web service.
      * 
      * @description //TODO
      * @example //TODO
-     * @param {HTMLElement} button The button which is clicked by the user.
+     * @param {HTMLElement} button The button that is clicked by the user.
      */
-    function sendFile(button) {
+    function onFileChange(button) {
         var file = document.querySelector("#js-file");
         button.onclick = function () {
             file.click();
         };
         file.addEventListener("change", function () {
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "https://api.imagga.com/v2/colors");
-            xhr.onload = function (event) {
-
-            };
-            xhr.setRequestHeader("authorization", "Basic YWNjX2VhMWFiMDhmM2JjMmI0NjpmNjU1NTRjMjlkZmU2MjBkZjZkNzlhOWI4NTZjOGRjYQ==");
             var formData = new FormData;
             formData.append("image", file.files[0]);
-            xhr.send(formData);
+            queryWebService("POST", null, formData);
         });
     }
 
-    function sendUrl(form) {
+    /**
+     * On url input, send the file to the web service.
+     * 
+     * @description //TODO
+     * @example //TODO
+     * @param {HTMLFormElement} form The form that is submitted.
+     */
+    function onFormSubmit(form) {
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+            queryWebService("GET", form.elements.url.value, null);
+        });
+    }
+
+    /**
+     * Connect to the web service to get images colors.
+     * 
+     * @description //TODO
+     * @example //TODO
+     * @param {string} method The used to connect to the web service.
+     * @param {string} query The complete query to append at the end of the url
+     * e.g "&Yay".
+     * @param {string} body The value that will go in the xhr.send() statement.
+     */
+    function queryWebService(method, query, body) {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://api.imagga.com/v2/colors?image_url=" + form.elements.url.value);
+        if (null === query) {
+            xhr.open(
+                method,
+                "https://api.imagga.com/v2/colors?extract_object_colors=0"
+            );
+        } else {
+            xhr.open(
+                method,
+                "https://api.imagga.com/v2/colors?extract_object_colors=0&image_url="
+                + query
+            );
+        }
+        xhr.setRequestHeader(
+            "authorization",
+            "Basic YWNjX2VhMWFiMDhmM2JjMmI0NjpmNjU1NTRjMjlkZmU2MjBkZjZkNzlhOWI4NTZjOGRjYQ=="
+        );
         xhr.onload = function (event) {
-            var response = JSON.parse(xhr.response);
-            console.log(response);
+            if (200 === xhr.status) {
+                var response = JSON.parse(xhr.response);
+                pushImage(query, response.result.colors.image_colors);
+                displayImages();
+                previewImage();
+            }
         };
-        xhr.setRequestHeader("authorization", "Basic YWNjX2VhMWFiMDhmM2JjMmI0NjpmNjU1NTRjMjlkZmU2MjBkZjZkNzlhOWI4NTZjOGRjYQ==");
-        xhr.send();
+        xhr.send(body);
     }
 
     /**
@@ -156,20 +193,15 @@
      * 
      * @description //TODO
      * @example //TODO
-     * @param {Array} images The gallery.
-     * @param {string} name The name of the image.
-     * @param {string} path The (relative or absolute) path to the file.
-     * @param {string} extension The file extension beginning with a dot e.g ".jpg" ".gif".
-     * @param {number} num The number of images we want push.
+     * @param {string} url The (relative or absolute) path to the file.
+     * @param {Object} colors The colors of the image.
      */
-    function pushImage(images, name, path, extension, num) {
-        for (var index = 0; index < num; index++) {
-            images.push({
-                name: name + (index + 1),
-                url: path + "image" + index + extension,
-                extension: extension
-            });
-        }
+    function pushImage(url, colors) {
+        images.push({
+            name: url,
+            url: url,
+            colors: colors
+        });
     }
 
     /**
@@ -178,7 +210,9 @@
      * @description //TODO
      * @example //TODO
      * @param {string} image The thing to be checked.
-     * @returns {boolean} Returns "yes" if the extension is valid (i.e is present in the validExtensions list) and "no" if the extension is invalid.
+     * @returns {boolean} Returns "yes" if the extension is valid
+     * i.e is present in the validExtensions list) and "no" if the extension
+     * is invalid.
      */
     function isExtensionValid(image) {
         for (var key in validExtensions) {
@@ -194,24 +228,24 @@
      *
      * @description //TODO
      * @example //TODO
-     * @param {Array} images The list of images.
      */
-    function displayImages(images) {
+    function displayImages() {
+        var gallery = document.querySelector(".js-gallery");
+        gallery.innerHTML = "";
         for (var key in images) {
-            if (isExtensionValid(images[key])) {
-                var galleryItem = insertElement("a", "", ".gallery");
-                var deleteText = document.createTextNode("x");
-                var deleteBtn = document.createElement("button");
-                deleteBtn.appendChild(deleteText);
-                galleryItem.appendChild(deleteBtn);
-                galleryItem.className = "image-item box-shadow transition";
-                galleryItem.dataUrl = images[key].url;
-                galleryItem.alt = images[key].name;
-                galleryItem.title = images[key].name;
-                galleryItem.style = "background-image: url(" + images[key].url + ")";
-                previewImage(galleryItem, images[key]);
-                deleteImage(deleteBtn, images[key]);
-            }
+            // if (isExtensionValid(images[key])) {}
+            var galleryItem = insertElement("a", "", ".js-gallery");
+            var deleteText = document.createTextNode("x");
+            var deleteBtn = document.createElement("button");
+            deleteBtn.appendChild(deleteText);
+            galleryItem.appendChild(deleteBtn);
+            galleryItem.className = "image-item box-shadow transition";
+            galleryItem.setAttribute("dataUrl", images[key].url);
+            galleryItem.alt = images[key].name;
+            galleryItem.title = images[key].name;
+            galleryItem.style = "background-image: url(" + images[key].url + ")";
+            galleryItem.addEventListener("click", previewImage(galleryItem));
+            deleteImage(deleteBtn, images[key]);
         }
     }
 
@@ -220,17 +254,13 @@
      *
      * @description //TODO
      * @example //TODO
-     * @param {string} container The image that will be listened.
      * @param {Object} image The object containing the details of the clicked image.
      */
-    function previewImage(container, image) {
-        container.addEventListener("click", function (event) {
-            var preview = document.querySelector(".js-image-preview");
-            preview.src = image.url;
-            preview.className = "clicked js-image-preview";
-            preview.addEventListener("click", removePreview);
-            // console.error("test");
-        });
+    function previewImage(image) {
+        var preview = document.querySelector(".js-image-preview");
+        preview.src = image.getAttribute("dataUrl") ;
+        preview.className = "clicked js-image-preview";
+        preview.addEventListener("click", removePreview);
     }
 
     /**
@@ -278,13 +308,14 @@
      *
      * @description //TODO
      * @example //TODO
-     * @param {number} i The number of images to pick from (i.e 5 means 6 backgrounds to pick from).
+     * @param {number} i The number of images to pick from
+     * (i.e 5 means 6 backgrounds to pick from).
      */
     function randomBackground(i) {
         var imageNumber = getRandomInt(i);
         var backgroundPath = "assets/background/";
         var background = document.querySelector("aside");
-        background.setAttribute("style", "background-image:url(" + backgroundPath + imageNumber + ".webp)");
+        background.style = "background-image:url(" + backgroundPath + imageNumber + ".webp)";
     }
 
     /**
@@ -311,26 +342,26 @@
         }
     }
 
-    function findIp() {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://ipinfo.io/json");
-        xhr.setRequestHeader("token", "4adcfde13a04f7");
-        xhr.onload = function (event) {
-            var response = JSON.parse(xhr.responseText);
-            console.log(response.city);
-        };
-        xhr.send();
-    }
+    // function findIp() {
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.open("GET", "https://ipinfo.io/json");
+    //     xhr.setRequestHeader("token", "4adcfde13a04f7");
+    //     xhr.onload = function (event) {
+    //         var response = JSON.parse(xhr.responseText);
+    //         console.log(response.city);
+    //     };
+    //     xhr.send();
+    // }
 
     displayMenu(menuItems);
-    pushImage(images, "Landscape ", "uploads/landscape/", ".jpg", 5);
     displayUpload(10, images);
     displayImages(images);
     randomBackground(5);
     displayTypes();
     // findIp();
 })
-/**
-    * Execute the anonymous function
-    */
-();
+
+    /**
+        * Execute the anonymous function
+        */
+    ();
