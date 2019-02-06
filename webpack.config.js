808a788711c734a54cf56bb941c3d0a33c74093b
Gaspacchio <gaspard.bariller@tutanota.com>
2019-02-06 10:56:08 +0100

    Fixed permission error on linux

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 module.exports = {
   entry: ['./src/index.js', './src/main.scss'],
   output: {
-    path: path.resolve(__dirname, '/dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
   },
   module: {
